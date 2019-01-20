0fd4669f51523c3267667cbc4e7730f8e02e7a66
Michael Zalla <michael@zalla.io>
2019-01-19 17:47:12 -0800

    Solves Problem 4.6;

--- epi_judge_python/primitive_divide.py
+++ epi_judge_python/primitive_divide.py
@@ -1,10 +1,30 @@
 from test_framework import generic_test
 
-
 def divide(x: int, y: int) -> int:
-    # TODO - you fill in here.
-    return 0
 
+    quotient = 0
+
+    power_of_two = 32
+
+    y_to_power = y << power_of_two
+
+    while x >= y:
+
+        # Finds the largest integer of the form y*(2**power_of_two) | integer < x
+
+        while y_to_power > x:
+
+            power_of_two -= 1
+
+            y_to_power >>= 1
+
+        # Removes y_to_power from x, adding (2**power_of_two) to our quotient;
+
+        x -= y_to_power
+
+        quotient += (1 << power_of_two)
+
+    return quotient
 
 if __name__ == '__main__':
     exit(
