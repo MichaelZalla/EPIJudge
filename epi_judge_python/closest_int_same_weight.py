1474b8431ecdfd58dde0694afff260e5f799356f
Michael Zalla <michael@zalla.io>
2019-01-19 17:40:39 -0800

    Solves Problem 4.4;

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,13 +1,20 @@
 from test_framework import generic_test
 
+def closest_binary_weighted_integer(x: int) -> int:
 
-def closest_int_same_bit_count(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+	NUM_UNSINGED_BITS = 64
 
+	for i in range(0, NUM_UNSINGED_BITS - 1):
+
+		# Isolates bit values for comparison
+		if (x >> i) & 1 != (x >> (i + 1)) & 1:
+
+			return x ^ ((1 << i) | (1 << (i + 1)))
+
+	raise ValueError("All bits are zero or one!")
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('closest_int_same_weight.py',
                                        'closest_int_same_weight.tsv',
-                                       closest_int_same_bit_count))
+                                       closest_binary_weighted_integer))
