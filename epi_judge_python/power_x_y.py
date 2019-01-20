82638755d687f9e42664ba1afaa5fbdbe4b2f36a
Michael Zalla <michael@zalla.io>
2019-01-19 17:52:59 -0800

    Solves Problem 4.7;

--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,10 +1,38 @@
 from test_framework import generic_test
 
-
 def power(x: float, y: int) -> float:
-    # TODO - you fill in here.
-    return 0.0
 
+    result = 1.0
+
+    # Adjusts our starting values if y is negative
+
+    if y < 0:
+        y = -y
+        x = 1.0 / x
+
+    # We'll "reduce" our y by powers-of-two, and double 'product' as we visit
+    # each digit position in y
+
+    product = x
+
+    # Our y will eventually be right-shifted to zero
+
+    while y:
+
+        # If the current bit in y is set, multiply by our running product
+
+        if y & 1:
+            result *= product
+
+        # Doubles our running multiple of x for the current digit postion
+
+        product = product*product
+
+        # Right-shifts our y
+
+        y >>= 1
+
+    return result
 
 if __name__ == '__main__':
     exit(generic_test.generic_test_main('power_x_y.py', 'power_x_y.tsv',
