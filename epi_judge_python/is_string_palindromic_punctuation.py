a498f18d5107e9fad45e4d4b97a71bd6fd62c91f
Michael Zalla <michael@zalla.io>
2019-01-26 16:17:25 -0800

    Solves Problem 6.5;

--- epi_judge_python/is_string_palindromic_punctuation.py
+++ epi_judge_python/is_string_palindromic_punctuation.py
@@ -1,10 +1,39 @@
 from test_framework import generic_test
 
+def is_palindrome(S: str) -> bool:
 
-def is_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
+    left, right = 0, len(S) - 1
+
+    while right > left:
+
+        # Advance left and right positions
+
+        while left < right and not S[left].isalnum():
+            left += 1
+
+        while right > left and not S[right].isalnum():
+            right -= 1
+
+        # Compare alphanumeric values
+
+        if S[left].lower() != S[right].lower():
+            return False
+
+        # Advance again
+
+        left += 1
+        right -= 1
+
+    # Assume palindromicity
+
     return True
 
+# print(is_palindrome('A man, a plan, a canal, Panama.'))
+# print(is_palindrome('Ray a Ray'))
+# print(is_palindrome('Able was I, ere I saw Elba!'))
+# print(is_palindrome('Not a palindrome.'))
+
+# exit()
 
 if __name__ == '__main__':
     exit(
