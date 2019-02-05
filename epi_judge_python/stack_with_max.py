550015c25f943d46da4610cbad9b31fc2dd6a32e
Michael Zalla <michael@zalla.io>
2019-02-05 09:18:36 -0800

    Solves Problem 8.1;

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -3,22 +3,95 @@ from test_framework.test_failure import TestFailure
 
 
 class Stack:
-    def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
 
-    def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+    class MaxWithCount:
 
-    def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        def __init__(self, max, count):
+            self.max, self.count = max, count
 
-    def push(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+    def __init__(self):
 
+        self._elements = []
+
+        self._maximums = []
+
+    def empty(self):
+
+        return len(self._elements) == 0
+
+    def max(self):
+
+        if self.empty():
+            return IndexError('pop(): Stack is empty!')
+
+        return self._maximums[-1].max
+
+    def pop(self):
+
+        if self.empty():
+            return IndexError('pop(): Stack is empty!')
+
+        n = self._elements.pop()
+        current_max = self._maximums[-1].max
+
+        if n == current_max:
+
+            self._maximums[-1].count -= 1
+
+            if self._maximums[-1].count == 0:
+                self._maximums.pop()
+
+        return n
+
+    def push(self, n):
+
+        self._elements.append(n)
+
+        if len(self._maximums) == 0:
+            self._maximums.append(self.MaxWithCount(n, 1))
+
+        else:
+
+            current_max = self._maximums[-1].max
+
+            if n == current_max:
+                self._maximums[-1].count += 1
+
+            elif n > current_max:
+                self._maximums.append(self.MaxWithCount(n, 1))
+
+# s = Stack()
+
+# assert(s.empty())
+
+# s.push(1)
+
+# assert(not s.empty())
+# assert(s.max() == 1)
+
+# s.pop()
+
+# assert(s.empty())
+
+# s.push(4)
+# s.push(6)
+# s.push(2)
+
+# assert(s.max() == 6)
+
+# s.pop()
+
+# assert(s.max() == 6)
+
+# s.pop()
+
+# assert(s.max() == 4)
+
+# s.pop()
+
+# assert(s.empty())
+
+# exit()
 
 def stack_tester(ops):
     try:
