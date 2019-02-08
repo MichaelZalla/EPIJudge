e3db78db2821fa95ba13d22861c20b403c454abb
Michael Zalla <michael@zalla.io>
2019-02-08 07:57:53 -0800

    Solves Problem 8.8;

--- epi_judge_python/circular_queue.py
+++ epi_judge_python/circular_queue.py
@@ -3,21 +3,73 @@ from test_framework.test_failure import TestFailure
 
 
 class Queue:
-    def __init__(self, capacity: int) -> None:
-        # TODO - you fill in here.
-        return
 
-    def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+    def __init__(self, capacity: int) -> None:
+
+        self._size = self.start = self.next = 0
+
+        self.L = [None] * capacity
+
+
+    def enqueue(self, elem: int) -> None:
+
+        # Checks whether there is a vacancy before start in the existing list
+
+        if self.size() < len(self.L):
+
+            # Fills in the vacant end position and advances next pointer
+
+            self.L[self.next] = elem
+
+            self.next = (self.next + 1) % len(self.L)
+
+        else:
+
+            # Otherwise, the current list is full
+
+            if self.start == 0:
+
+                # If the list begins at the first index position, simply append
+                # a new item at the end
+
+                self.L = self.L + [elem]
+
+            else:
+
+                # Otherwise, we need to shift L[start:] right by one, making a
+                # new position vacant at L[next]
+
+                self.L = self.L + [None]
+
+                for i in reversed(range(self.start, len(self.L) -1)):
+                    self.L[i+1] = self.L[i]
+
+                self.L[self.next] = elem
+
+                self.start, self.next = self.start + 1, self.next + 1
+
+        # Increments size
+
+        self._size += 1
+
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+
+        if not self.size():
+            return IndexError('dequeue: Called on an empty queue!')
+
+        value = self.L[self.start]
+
+        # Advances the start pointer by one position, and incremeents size
+
+        self.start = (self.start + 1) % len(self.L)
+
+        self._size -= 1
+
+        return value
 
     def size(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        return self._size
 
 
 def queue_tester(ops):
