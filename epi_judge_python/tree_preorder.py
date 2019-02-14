28fae8f33534e9282cc66ad77d12d95e4cfda8f7
Michael Zalla <michael@zalla.io>
2019-02-13 19:29:57 -0800

    Solves Problem 9.8;

--- epi_judge_python/tree_preorder.py
+++ epi_judge_python/tree_preorder.py
@@ -3,10 +3,43 @@ from typing import List
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
+from binary_tree_node import BinaryTreeNode
 
-def preorder_traversal(tree: BinaryTreeNode) -> List[int]:
-    # TODO - you fill in here.
-    return []
+def preorder_traversal(root: BinaryTreeNode) -> List[int]:
+
+    if not root:
+        return []
+
+    stack, result = [root], []
+
+    while stack:
+
+        node = stack.pop()
+
+        result.append(node.data)
+
+        if node.right:
+            stack.append(node.right)
+
+        if node.left:
+            stack.append(node.left)
+
+    return result
+
+# H = BinaryTreeNode('H')
+# F = BinaryTreeNode('F', None, H)
+# E = BinaryTreeNode('E')
+# C = BinaryTreeNode('C', E, F)
+
+
+# G = BinaryTreeNode('G')
+# D = BinaryTreeNode('D', G)
+# B = BinaryTreeNode('B', None, D)
+# A = BinaryTreeNode('A', B, C)
+
+# print(preorder_traversal(A))
+
+# exit()
 
 
 if __name__ == '__main__':
