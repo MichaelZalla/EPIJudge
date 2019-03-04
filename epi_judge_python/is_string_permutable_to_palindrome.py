6c6c792ed0be990f1c8e89c735c0037530e5c85e
Michael Zalla <michael@zalla.io>
2019-03-03 19:52:09 -0800

    Solves Problem 12.1;

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -1,10 +1,25 @@
 from test_framework import generic_test
 
+from collections import Counter
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
 
+    counts = Counter(s)
+
+    return len(list(filter(lambda count: count % 2 == 1, counts.values()))) < 2
+
+
+# s1 = 'level'
+# s2 = 'rotator'
+# s3 = 'foobaraboof'
+# s4 = 'edified'
+
+# print(s1, can_form_palindrome(s1))
+# print(s2, can_form_palindrome(s2))
+# print(s3, can_form_palindrome(s3))
+# print(s4, can_form_palindrome(s4))
+
+# exit()
 
 if __name__ == '__main__':
     exit(
