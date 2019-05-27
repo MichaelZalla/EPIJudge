61305987fb437cb078bfc3daa656f860487fac09
Michael Zalla <michael@zalla.io>
2019-05-26 20:44:56 -0700

    Solves Problem 13.6;

--- epi_judge_python/interval_add.py
+++ epi_judge_python/interval_add.py
@@ -8,12 +8,37 @@ from test_framework.test_utils import enable_executor_hook
 
 Interval = collections.namedtuple('Interval', ('left', 'right'))
 
+def overlaps(interval1, interval2):
+    return (interval1.left <= interval2.right and interval1.right >= interval2.left)
 
 def add_interval(disjoint_intervals: List[Interval],
                  new_interval: Interval) -> List[Interval]:
-    # TODO - you fill in here.
-    return []
 
+    overlapping = [interval for interval in disjoint_intervals if overlaps(interval, new_interval)]
+
+    overlapping.append(new_interval)
+
+    start = min([interval.left for interval in overlapping])
+    finish = max([interval.right for interval in overlapping])
+
+    # start = min(list(map(lambda x: x.left, overlapping)))
+    # finish = max(list(map(lambda x: x.right, overlapping)))
+
+    return \
+        [interval for interval in disjoint_intervals if interval.right < start] + \
+        [Interval(start, finish)] +\
+        [interval for interval in disjoint_intervals if interval.left > finish]
+
+# I0 = []
+# E0 = Interval(4,8)
+
+# I1 = [Interval(1,2), Interval(3,4), Interval(6,7)]
+# E1 = Interval(4,8)
+
+# print(add_interval(I0, E0))
+# print(add_interval(I1, E1))
+
+# exit()
 
 @enable_executor_hook
 def add_interval_wrapper(executor, disjoint_intervals, new_interval):
