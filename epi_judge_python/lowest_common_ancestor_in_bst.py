7358fce472e1080e95ce3bcee55c000b2dc23cd5
Michael Zalla <michael@zalla.io>
2019-05-29 21:06:53 -0700

    Solves Problem 14.4;

--- epi_judge_python/lowest_common_ancestor_in_bst.py
+++ epi_judge_python/lowest_common_ancestor_in_bst.py
@@ -9,15 +9,24 @@ from test_framework.test_utils import enable_executor_hook
 
 
 # Input nodes are nonempty and the key at s is less than or equal to that at b.
-def find_lca(tree: BstNode, s: BstNode, b: BstNode) -> Optional[BstNode]:
-    # TODO - you fill in here.
+def find_lowest_common_ancestor(root: BstNode, min_node: BstNode, max_node: BstNode) -> Optional[BstNode]:
+
+    while root:
+
+        if min_node.data <= root.data <= max_node.data:
+            return root
+        elif max_node.data < root.data:
+            root = root.left
+        else:
+            root = root.right
+
     return None
 
 
 @enable_executor_hook
 def lca_wrapper(executor, tree, s, b):
     result = executor.run(
-        functools.partial(find_lca, tree, must_find_node(tree, s),
+        functools.partial(find_lowest_common_ancestor, tree, must_find_node(tree, s),
                           must_find_node(tree, b)))
     if result is None:
         raise TestFailure('Result can\'t be None')
