b1325acf905a84e73a574ec5451e1092f378eb1e
Michael Zalla <michael@zalla.io>
2019-06-30 15:45:02 -0700

    Solves Problem 17.7;

--- epi_judge_python/max_trapped_water.py
+++ epi_judge_python/max_trapped_water.py
@@ -3,9 +3,38 @@ from typing import List
 from test_framework import generic_test
 
 
-def get_max_trapped_water(heights: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+def get_max_trapped_water(S: List[int]) -> int:
+
+    i, j, running_max_capacity = 0, len(S) - 1, 0
+
+    while i < j:
+
+        height = min(S[i], S[j])
+
+        capacity = height * (j-i)
+
+        if capacity > running_max_capacity:
+            running_max_capacity = capacity
+
+        # Move in, looking for a higher-capacity pair
+
+        while i < j and S[i] <= height:
+            i += 1
+
+        while i < j and S[j] <= height:
+            j -= 1
+
+    return running_max_capacity
+
+# print(get_max_trapped_water([1,2]))
+# print(get_max_trapped_water([2,1]))
+# print(get_max_trapped_water([1,2,3]))
+# print(get_max_trapped_water([3,2,1]))
+# print(get_max_trapped_water([1,2,2,1]))
+# print(get_max_trapped_water([1,2,2,2,1]))
+# print(get_max_trapped_water([1,2,1,3,4,4,5,6,2,1,3,1,3,2,1,2,4,1]))
+
+# exit()
 
 
 if __name__ == '__main__':
