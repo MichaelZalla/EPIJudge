737531a60aa45352a8382f7c25c7ddccdd924b80
Michael Zalla <michael@zalla.io>
2019-07-04 16:04:34 -0700

    Solves Problem 5.10;

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -3,9 +3,48 @@ from typing import List
 from test_framework import generic_test
 
 
-def apply_permutation(perm: List[int], A: List[int]) -> None:
-    # TODO - you fill in here.
+def apply_permutation(P: List[int], A: List[int]) -> None:
+
+    for i in range(len(A)):
+
+        # Element A[i] will be moving to index P[i]
+
+        next = i
+
+        # While P[next] points to an unvisited element in this cycle...
+
+        while P[next] >= 0:
+
+            # Move A[i] into A[P[next]]; temporarily store whatever value was at
+            # A[P[next]] at the 'hole' created at A[i];
+
+            A[i], A[P[next]] = A[P[next]], A[i]
+
+            # Store P[next] (next destination index) temporarily before we
+            # overwrite P[next]; we use a constant offset to maintain the
+            # original, relative ordering of P-indices while simultaneously
+            # marking the P-index as 'completed';
+
+            temp = P[next]
+
+            P[next] -= len(P)
+
+            next = temp
+
+    # Restores the original indices in P by adding back the constant offset;
+
+    P[:] = [i + len(P) for i in P]
+
     return
+
+
+# A, P = ['a','b','c','d',], [2,0,1,3]
+
+# apply_permutation(P, A)
+
+# print(A, P)
+
+# exit()
 
 
 def apply_permutation_wrapper(perm, A):
