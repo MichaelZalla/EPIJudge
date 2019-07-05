f539431921cccde093885164341806c54dfa9829
Michael Zalla <michael@zalla.io>
2019-07-05 11:21:16 -0700

    Solves Problem 8.9;

--- epi_judge_python/queue_from_stacks.py
+++ epi_judge_python/queue_from_stacks.py
@@ -2,14 +2,66 @@ from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
-class Queue:
-    def enqueue(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+class QueueNaive:
+
+    def __init__(self):
+        self.queue = []
+        self.temp = []
+
+    def enqueue(self, elem: int) -> None:
+
+        while self.queue:
+            self.temp.append(self.queue.pop())
+
+        self.queue.append(elem)
+
+        while self.temp:
+            self.queue.append(self.temp.pop())
 
     def dequeue(self) -> int:
-        # TODO - you fill in here.
-        return 0
+
+        if not self.queue:
+            raise IndexError('Tries to dequeu from empty queue!')
+
+        return self.queue.pop()
+
+
+class Queue:
+
+    def __init__(self):
+        self.en, self.de = [], []
+
+    def enqueue(self, elem: int) -> None:
+
+        self.en.append(elem)
+
+    def dequeue(self) -> int:
+
+        if not self.de:
+            while self.en:
+                self.de.append(self.en.pop())
+
+        if not self.de:
+            raise IndexError('Tries to dequeu from empty queue!')
+
+        return self.de.pop()
+
+
+# q = Queue()
+
+# q.enqueue('A')
+# q.enqueue('B')
+# q.enqueue('C')
+
+# print(q.dequeue())
+# print(q.dequeue())
+
+# q.enqueue('D')
+
+# print(q.dequeue())
+# print(q.dequeue())
+
+# exit()
 
 
 def queue_tester(ops):
