b0d9f17f7774ebc9b4c473f97536a9c232ff7396
Michael Zalla <michael@zalla.io>
2019-07-04 19:56:42 -0700

    Solves Problem 6.7;

--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -2,10 +2,52 @@ from typing import List
 
 from test_framework import generic_test, test_utils
 
+M = {}
 
-def phone_mnemonic(phone_number: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+M['0'] = '0'
+M['1'] = '1'
+M['2'] = 'ABC'
+M['3'] = 'DEF'
+M['4'] = 'GHI'
+M['5'] = 'JKL'
+M['6'] = 'MNO'
+M['7'] = 'PQRS'
+M['8'] = 'TUV'
+M['9'] = 'WXYZ'
+
+def phone_mnemonic(S: str) -> List[str]:
+
+    def add_mnemonics(S, index, partial_result):
+
+        if index == len(S):
+
+            results.append(''.join(partial_result))
+
+            return
+
+        digit = S[index]
+
+        letters = M[digit]
+
+        for letter in letters:
+
+            partial_result[index] = letter
+
+            add_mnemonics(S, index + 1, partial_result)
+
+            # partial_result = partial_result[:-1]
+
+    results, partial_result = [], [0] * len(S)
+
+    add_mnemonics(S, 0, partial_result)
+
+    return results
+
+
+# print(phone_mnemonic('859'))
+# print(phone_mnemonic('2276696'))
+
+# exit()
 
 
 if __name__ == '__main__':
