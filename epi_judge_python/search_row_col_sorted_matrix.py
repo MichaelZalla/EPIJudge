3df3d0ab80b453579cad69d2d9a5627c38c12e59
Michael Zalla <michael@zalla.io>
2019-07-05 16:51:36 -0700

    Solves Problem 11.6;

--- epi_judge_python/search_row_col_sorted_matrix.py
+++ epi_judge_python/search_row_col_sorted_matrix.py
@@ -4,8 +4,32 @@ from test_framework import generic_test
 
 
 def matrix_search(A: List[List[int]], x: int) -> bool:
-    # TODO - you fill in here.
-    return True
+
+    row, col = 0, len(A[0]) - 1
+
+    # Row index will increase during search, column index will decrease;
+
+    while row < len(A) and col > -1:
+
+        if A[row][col] < x:
+
+            # We can ignore this entire row
+
+            row += 1
+
+        elif A[row][col] > x:
+
+            # We can ignore this entire column
+
+            col -= 1
+
+        else:
+
+            return True
+
+    # We exhausted all rows and columns without finding x
+
+    return False
 
 
 if __name__ == '__main__':
