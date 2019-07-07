02b64dcd0009f47f22dd03c2b19c3edbe891be96
Michael Zalla <michael@zalla.io>
2019-07-07 11:22:06 -0700

    Solves Problem 12.10;

--- epi_judge_python/longest_contained_interval.py
+++ epi_judge_python/longest_contained_interval.py
@@ -4,8 +4,55 @@ from test_framework import generic_test
 
 
 def longest_contained_range(A: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    # Build a lookup table for all unique integers in A
+
+    D, max_interval = {}, 0
+
+    for value in A:
+        D[value] = value
+
+    # Iterate over the entries in D, considering each integer value as though it
+    # were the final integer in some interval in A (i.e., largest integer in the
+    # interval);
+
+    # List allows us to modify the set of keys in D during our pass;
+
+    for key in list(D.keys()):
+
+        # We may have deleted this key during our pass
+
+        if not key in D:
+            continue
+
+        # Can we 'connect' our integer to the interval ending with its
+        # predecessor (i.e., value - 1)?
+
+        next = D[key] - 1
+
+        while next in D:
+
+            # Merges the two intervals, deleting 'next' from our table;
+
+            D[key] = D[next]
+
+            del D[next]
+
+            next = D[key] - 1
+
+        # Check if this value's current interval is the largest we've seen;
+
+        interval = key - D[key] + 1
+
+        if interval > max_interval:
+            max_interval = interval
+
+    return max_interval
+
+
+# print(longest_contained_range([3,-2,7,9,8,1,2,0,-1,5,8]))
+
+# exit()
 
 
 if __name__ == '__main__':
