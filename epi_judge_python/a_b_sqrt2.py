587df4caa4b496a75ca17661b0cc13652ea7eb2b
Michael Zalla <michael@zalla.io>
2019-07-07 20:53:08 -0700

    Solves Problem 14.7;

--- epi_judge_python/a_b_sqrt2.py
+++ epi_judge_python/a_b_sqrt2.py
@@ -2,10 +2,56 @@ from typing import List
 
 from test_framework import generic_test
 
+import math
+import bintrees
+
+
+def generate_first_k_a_b_sqrt2_naive(k):
+
+    tree = bintrees.AVLTree()
+
+    for a in range(0, k + 1):
+        tree.insert(a, (a, 0))
+
+    for b in range(1, k + 1):
+
+        a = 0
+
+        while a < k + 1 and a + b * math.sqrt(2) < tree.max_key():
+
+            tree.pop_max()
+
+            tree.insert(a + b * math.sqrt(2), (a, b))
+
+            a += 1
+
+    return [product for product, a_b_pair in tree.nsmallest(k)]
+
 
 def generate_first_k_a_b_sqrt2(k: int) -> List[float]:
-    # TODO - you fill in here.
-    return []
+
+    result, tree = [], bintrees.AVLTree()
+
+    tree.insert(0, (0, 0))
+
+    while len(result) < k:
+
+        min_node = tree.pop_min()
+
+        min_product, min_a_b = min_node[0], min_node[1]
+
+        result.append(min_product)
+
+        a, b = min_a_b[0], min_a_b[1]
+
+        tree.insert((a + 1) + b * math.sqrt(2), (a + 1, b))
+        tree.insert(a + (b + 1) * math.sqrt(2), (a, b + 1))
+
+    return result
+
+# print(generate_first_k_a_b_sqrt2_naive(5))
+
+# exit()
 
 
 if __name__ == '__main__':
