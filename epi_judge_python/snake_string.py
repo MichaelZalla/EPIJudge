458bb24d4a04f9f568b59f4d03db29387b6fc0e8
Michael Zalla <michael@zalla.io>
2019-07-10 22:35:59 -0700

    Solves Problem 6.11;

--- epi_judge_python/snake_string.py
+++ epi_judge_python/snake_string.py
@@ -1,12 +1,23 @@
 from test_framework import generic_test
 
 
-def snake_string(s: str) -> str:
-    # TODO - you fill in here.
-    return ''
+def snake_string(S: str) -> str:
 
+    return ''.join(
+        [S[i] for i in range(1, len(S), 4)] +
+        [S[i] for i in range(0, len(S), 2)] +
+        [S[i] for i in range(3, len(S), 4)]
+    )
+
+def snake_string_pythonic(S):
+
+    return S[1::4] + S[::2] + S[3::4]
+
+# print(snake_string('Hello World!'))
+
+# exit()
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('snake_string.py', 'snake_string.tsv',
-                                       snake_string))
+                                       snake_string_pythonic))
