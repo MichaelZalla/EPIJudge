45bd9aac96e8ff02f228e110941a864335bab73e
Michael Zalla <michael@zalla.io>
2019-08-12 22:34:21 -0700

    Solves Problem 6.3;

--- epi_judge_python/spreadsheet_encoding.py
+++ epi_judge_python/spreadsheet_encoding.py
@@ -1,9 +1,39 @@
+import functools
+
 from test_framework import generic_test
 
+BASE = 26
+
+def get_base26_digit(c):
+
+    return ord(c) - ord('A') + 1
 
 def ss_decode_col_id(col: str) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    n = len(col)
+
+    return functools.reduce(
+        lambda sum, index: sum + get_base26_digit(col[n - 1 - index]) * (BASE**index),
+        list(range(len(col))),
+        0
+    )
+
+
+# print('A', ss_decode_col_id('A'))
+# print('B', ss_decode_col_id('B'))
+# print('C', ss_decode_col_id('C'))
+# print('Z', ss_decode_col_id('Z'))
+# print('AA', ss_decode_col_id('AA'))
+# print('AB', ss_decode_col_id('AB'))
+# print('AC', ss_decode_col_id('AC'))
+# print('YY', ss_decode_col_id('YY'))
+# print('ZZ', ss_decode_col_id('ZZ'))
+# print('AAA', ss_decode_col_id('AAA'))
+# print('AAB', ss_decode_col_id('AAB'))
+# print('AAC', ss_decode_col_id('AAC'))
+# print('BBB', ss_decode_col_id('BBB'))
+
+# exit()
 
 
 if __name__ == '__main__':
