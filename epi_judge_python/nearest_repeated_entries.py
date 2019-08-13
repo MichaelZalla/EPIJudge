31217bf1380b8539a56fb2f4e396994ebe2d3840
Michael Zalla <michael@zalla.io>
2019-08-13 10:30:33 -0700

    Solves Problem 12.6;

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph: List[str]) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    words, min_distance = {}, -1
+
+    for index, word in enumerate(paragraph):
+
+        if word not in words:
+            words[word] = (index, len(paragraph))
+
+        else:
+
+            prev_distance = words[word][1]
+            curr_distance = index - words[word][0]
+
+            words[word] = (index, min(prev_distance, curr_distance))
+
+            min_distance = curr_distance if min_distance == -1 else \
+                min(min_distance, curr_distance)
+
+    return min_distance
+
+
 
 
 if __name__ == '__main__':
