c38a096b3f6908f60cfed85f1138723d69cfa20e
Michael Zalla <michael@zalla.io>
2019-08-12 22:57:32 -0700

    Solves Problem 6.10;

--- epi_judge_python/valid_ip_addresses.py
+++ epi_judge_python/valid_ip_addresses.py
@@ -3,9 +3,52 @@ from typing import List
 from test_framework import generic_test
 
 
+def is_valid_ip_address_component(c: str) -> bool:
+
+    d = int(c)
+
+    if len(c) > len(str(d)):
+        return False
+
+    if d > 255:
+        return False
+
+    return True
+
 def get_valid_ip_address(s: str) -> List[str]:
-    # TODO - you fill in here.
-    return []
+
+    result = []
+
+    for i in range(0, len(s) - 3):
+
+        a = s[0:i+1]
+
+        if not is_valid_ip_address_component(a):
+            continue
+
+        for j in range(i + 1, len(s) - 2):
+
+            b = s[i+1:j+1]
+
+            if not is_valid_ip_address_component(b):
+                continue
+
+            for k in range(j + 1, len(s) - 1):
+
+                c = s[j+1:k+1]
+                d = s[k+1:len(s)]
+
+                if not is_valid_ip_address_component(c) or not is_valid_ip_address_component(d):
+                    continue
+
+                result.append('{}.{}.{}.{}'.format(a, b, c, d))
+
+    return result
+
+
+# print(get_valid_ip_address('19216811'))
+
+# exit()
 
 
 def comp(a, b):
